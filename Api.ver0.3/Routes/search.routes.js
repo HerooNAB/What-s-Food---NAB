0654cbda915b388f1dc4bd93d296fd0f54ce5471
HerooNAB <56840074+HerooNAB@users.noreply.github.com>
2021-01-04 16:01:45 +0700

    Binh update searchuser + mypost

--- Api.ver0.3/Routes/search.routes.js
+++ Api.ver0.3/Routes/search.routes.js
@@ -5,10 +5,10 @@ const Bag = require('../models/Bag.model.js');
 require('dotenv').config()
 
 router.get("/searchuser", async (req,res) => {
-    const query = "^" + req.query.user + "$";
+    const query = "^" + req.query.name + "$";
     console.log(query.toString());
     const fountUser = await User.find({
-        name: {$regex: req.query.user, $options: "i"},
+        name: {$regex: req.query.name, $options: "i"},
     });
     if (!fountUser) return res.status(000).send({message: "khong tim thay"});
     res.status(200).send(fountUser);
